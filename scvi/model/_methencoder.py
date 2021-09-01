e18d8dedcad50b573abc13143f1f5337cba98c6e
Michael Scherer <mscherer@mpi-inf.mpg.de>
2021-09-01 11:36:50 +0200

    intermediate result

--- scvi/model/_methencoder.py
+++ scvi/model/_methencoder.py
@@ -49,77 +49,63 @@ class methVAE(nn.Module):
         
     def model(self, dat):
         x = dat['X']
-        pyro.module("decoder", self.decoder)
+#        pyro.module("decoder", self.decoder)
+        mean_nb1, mean_nb2, alpha, disp_nb1, disp_nb2 = torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1])-.1
+        pi_init   = torch.ones(torch.Size((x.shape[0], x.shape[1])))
+        mean_nb1 = pyro.param("mean_nb1", mean_nb1)
+        disp_nb2 = pyro.param("disp_nb2", disp_nb2)
+        disp_nb1 = pyro.param("disp_nb1", disp_nb1, constraint=constraints.greater_than(disp_nb2))
+        mean_nb2 = pyro.param("mean_nb2", mean_nb2)
+        pi_p = pyro.param("pi",pi_init)
         with pyro.plate("data", x.shape[0]):
-            mean_nb1, alpha_min, alpha_max, disp_nb1, disp_nb2  = torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.zeros(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1])))
-            gauss_mean = torch.zeros(torch.Size((x.shape[0], self.bottleneck_size)))
-            gauss_sd = torch.ones(torch.Size((x.shape[0], self.bottleneck_size)))
-            bottleneck = pyro.sample("bottleneck", dist.Normal(gauss_mean, gauss_sd).to_event(1))
-#            mean_nb1_sampled = pyro.sample("mean_nb", dist.HalfNormal(mean_nb1).to_event(1))
-#            alpha_sampled = pyro.sample("alpha", dist.Uniform(alpha_min, alpha_max).to_event(1))
-#            disp_nb1_sampled = pyro.sample("disp_nb1", dist.HalfNormal(disp_nb1).to_event(1))
-#            disp_nb2_sampled = pyro.sample("disp_nb2", dist.HalfNormal(disp_nb2).to_event(1))
-#            print(alpha_sampled)
-#            print(x.index)
-            mean_nb1_sampled = pyro.param("mean_nb1", self.mean_nb1)
-            mean_nb2_sampled = pyro.param("mean_nb2", self.mean_nb2)
-            mean_nb1_sampled = torch.exp(mean_nb1_sampled)
-            mean_nb2_sampled = torch.exp(mean_nb2_sampled)
-            alpha_sampled = pyro.sample("alpha", dist.Uniform(alpha_min,alpha_max))
-            alpha_sampled = torch.exp(alpha_sampled)/(1+torch.exp(alpha_sampled))
-#            enzyme_activity = pyro.param("enzyme_activity", torch.nn.Parameter(torch.randn(x.shape[0])), constraint=constraints.interval(0,1))
-#            enzyme_activity = enzyme_activity[range(0,x.shape[0])]
-#            print(mean_nb1_sampled)
-#            alpha_sampled = pyro.param("alpha", self.alpha, constraint=constraints.interval(0,1))
-#            print(alpha_sampled)
-#            pi_reconstructed = self.decoder(bottleneck)
-#            print(mean_nb1_sampled)
-#            print(mean_nb2_sampled)
-            pi = torch.nn.Parameter(torch.randn(x.shape[0]))
-            pi = pi.expand(x.shape[1], x.shape[0]).t()
-            pi = torch.exp(pi)/(1+torch.exp(pi))
-            pi_reconstructed = pyro.param("pi",dist.Bernoulli(pi).to_event(1))>0.5
-            disp_nb1 = pyro.param("disp_nb1", self.disp_nb1)
-            disp_nb1 = torch.exp(disp_nb1)
-            #disp_nb2 = pyro.param("disp_nb2", self.disp_nb2)
-            #disp_nb2 = torch.exp(disp_nb2)
-            #disp_nb2 = (disp_nb1*alpha_sampled)+self.eps
-            disp_nb1 = (disp_nb1)/(disp_nb1+mean_nb1_sampled)
-            #disp_nb2 = (disp_nb2)/(disp_nb2+mean_nb2_sampled)
-#            print(disp_nb1)
-#            print(disp_nb2)
-#            enzyme_activity = enzyme_activity.expand(x.shape[1], x.shape[0]).t()
-#            pi_reconstructed = pi_reconstructed*(1-enzyme_activity)
-#            pi_reconstructed = pi_reconstructed>0.5
-            dist_res = dist.MaskedMixture(pi_reconstructed,
-                          dist.NegativeBinomial(mean_nb1_sampled, disp_nb1),
-                          dist.NegativeBinomial(mean_nb2_sampled, (disp_nb1*alpha_sampled)+self.eps)).to_event(1)
+#            foreground = pyro.sample("foreground", dist.NegativeBinomial(mean_nb1,disp_nb1).to_event(1))
+#            background = pyro.sample("background", dist.NegativeBinomial(mean_nb2,disp_nb2).to_event(1))
+#            pi = pyro.param("pi",pi)
+            disp_nb1_s = pyro.sample("dispersion_nb1", dist.Exponential(disp_nb1).to_event(1))
+            disp_nb2_s = pyro.sample("dispersion_nb2", dist.Exponential(disp_nb2).to_event(1))
+            pi_reconconstructed = pyro.sample("pi_s", dist.Bernoulli(pi_p).to_event(1))
+#                bottleneck = pyro.sample("bottleneck_%d" % ampli, dist.Normal(gauss_mean, gauss_sd).to_event(1))
+            pi_reconconstructed = pi_reconconstructed>0.5
+#            pyro.sample("obs",
+#                        dist.NegativeBinomial(mean_nb1,disp_nb1).to_event(1),
+#                        obs=x)
+#            pyro.sample("obs_neg",
+#                        dist.NegativeBinomial(mean_nb2,disp_nb2).to_event(1),
+#                        obs=x)
+#                foreground = pyro.sample("foreground_%d" % ampli, dist.NegativeBinomial(mean_nb1, disp_nb1))
+#                background = pyro.sample("background_%d" % ampli, dist.NegativeBinomial(mean_nb2, (disp_nb1*alpha)+self.eps))
+            dist_res = dist.MaskedMixture(pi_reconconstructed,
+                          dist.NegativeBinomial(mean_nb1,disp_nb1_s),
+                          dist.NegativeBinomial(mean_nb2,disp_nb2_s))
             pyro.sample("obs", 
-                       dist_res,
+                       dist_res.to_event(1),
                        obs=x)
 
     def guide(self, dat):
         x = dat['X']
-        pyro.module("encoder", self.encoder)
+#        pyro.module("encoder", self.encoder)
+        mean_nb1, mean_nb2, alpha, disp_nb1, disp_nb2 = torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1]), torch.ones(x.shape[1])-.1
+        pi_init   = torch.ones(torch.Size((x.shape[0], x.shape[1])))
+        mean_nb1 = pyro.param("mean_nb1", mean_nb1)
+        mean_nb2 = pyro.param("mean_nb2", mean_nb2)
+#        mean_nb1 = mean_nb1.expand(x.shape[0], x.shape[1]).t()
+#        mean_nb2 = mean_nb2.expand(x.shape[0], x.shape[1]).t()
+#        alpha = pyro.param("alpha", alpha, constraint=constraints.interval(0,1))
+#        alpha = alpha.expand(x.shape[0], x.shape[1]).t()
+        disp_nb2 = pyro.param("disp_nb2", disp_nb2)
+        disp_nb1 = pyro.param("disp_nb1", disp_nb1, constraint=constraints.greater_than(disp_nb2))
+        pi_p = pyro.param("pi",pi_init)
+#        disp_nb1 = disp_nb1.expand(x.shape[0], x.shape[1]).t()
         with pyro.plate("data", x.shape[0]):
-            mean_nb1, alpha_min, alpha_max, disp_nb1, disp_nb2  = torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.zeros(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1]))), torch.ones(torch.Size((x.shape[0], x.shape[1])))
-            gauss_mean, gauss_scale, latent = self.encoder(x)
-            pyro.param("mean_nb1", self.mean_nb1)
-            pyro.param("mean_nb2", self.mean_nb2)
-            pyro.sample("alpha", dist.Uniform(alpha_min,alpha_max))
-#            pyro.param("enzyme_activity", torch.nn.Parameter(torch.randn(x.shape[0])), constraint=constraints.interval(0,1))
-#            pyro.param("alpha", self.alpha, constraint=constraints.interval(0,1))
-            pyro.param("disp_nb1", self.disp_nb1)
-#            pyro.param("disp_nb2", self.disp_nb2)
-#            pyro.sample("mean_nb", dist.HalfNormal(mean_nb1).to_event(1))
-#            pyro.sample("alpha", dist.Uniform(alpha_min, alpha_max).to_event(1))
-#            pyro.sample("disp_nb1", dist.HalfNormal(disp_nb1).to_event(1))
-#            pyro.sample("disp_nb2", dist.HalfNormal(disp_nb2).to_event(1))
-            pi = torch.nn.Parameter(torch.randn(x.shape[0]))
-            pi = pi.expand(x.shape[1], x.shape[0]).t()
-            pi = torch.exp(pi)/(1+torch.exp(pi))
-            pyro.param("pi",dist.Bernoulli(pi).to_event(1))>0.5
-            pyro.sample("bottleneck", dist.Normal(gauss_mean, gauss_scale).to_event(1))
+            pyro.sample("dispersion_nb1", dist.Exponential(disp_nb1).to_event(1))
+            pyro.sample("dispersion_nb2", dist.Exponential(disp_nb2).to_event(1))
+#            foreground = pyro.sample("foreground",dist.NegativeBinomial(mean_nb1,disp_nb1).to_event(1))
+#            background = pyro.sample("background",dist.NegativeBinomial(mean_nb2,disp_nb2).to_event(1))
+            pi_reconstructred = pyro.sample("pi_s",dist.Bernoulli(pi_p).to_event(1))
+            pi_reconstructred = pi_reconstructred>0.5
+#                pyro.sample("bottleneck_%d" % ampli, dist.Normal(gauss_mean, gauss_scale).to_event(1))
+#                pyro.sample("foreground_%d" % ampli, dist.NegativeBinomial(mean_nb1, disp_nb1))
+#                pyro.sample("background_%d" % ampli, dist.NegativeBinomial(mean_nb2, (disp_nb1*alpha)+self.eps))
             
     def generate_counts(self, x):
         gauss_mean, gauss_scale, latent = self.encoder(x)
